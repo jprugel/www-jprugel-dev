2402069e75131c3bab44875a0dd1107056ca48e6
jprugel <jonathon.prugel@gmail.com>
2024-05-27 21:14:29 -0400

    oops

--- client/src/components/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod article;
-pub mod feed;
-pub mod navbar;
-pub mod search;
-pub mod login;
--- server/src/pools.rs
+++
@@ -1,6 +0,0 @@
-use rocket_db_pools::diesel::PgPool;
-use rocket_db_pools::Database;
-
-#[derive(Database)]
-#[database("posts")]
-pub struct Db(PgPool);
